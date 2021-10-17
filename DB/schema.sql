9984751c4d713b2f1a971d1610b5bd5e808fff63
Ben Townsend <RBT000011@gmail.com>
2021-10-17 12:20:49 -0500

    added lines to schema for department, role and employee.

--- DB/schema.sql
+++ DB/schema.sql
@@ -3,7 +3,24 @@ CREATE DATABASE tracker_db;
 
 USE tracker_db;
 
-CREATE TABLE employee_names (
+
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  _name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE _role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (6,2),
+  department_id INT NOT NULL,
+)
+
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
 )
