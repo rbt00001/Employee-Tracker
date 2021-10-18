b5a20be5ebe6590cfa0b6d77b7c1daa12ff3e118
Ben Townsend <RBT000011@gmail.com>
2021-10-18 17:51:09 -0500

    dotenv added and server.js added

--- DB/schema.sql
+++ DB/schema.sql
@@ -21,6 +21,6 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
+  _role_id INT NOT NULL,
   manager_id INT NOT NULL
 )
